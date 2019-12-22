fa63916cea4effd42211fce73d43a917868299a7
Uma Manandhar <um_2003_8@hotmail.com>
2019-12-22 03:23:08 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 class Artist
   include Paramable
-  extend Memorable
+  extend Memorable::ClassMethods
+  include Memorable:: InstanceMethods
   extend Findable
 
   attr_accessor :name
